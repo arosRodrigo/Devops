f60fa4f7ecd236b1ba00d05c5363ca74f9a7c707
arosRodrigo <an.rodrigo.os@gmail.com>
2019-07-15 18:41:30 -0300

    exec1-radar KM resolvido

--- Python/para_Zumbis/exec1-radar KM.py
+++ Python/para_Zumbis/exec1-radar KM.py
@@ -2,3 +2,12 @@
     perguntar a velocidade de um carro. Se o mesmo ultrapassou 110 Km,
     exibir mensagem informando que o usuário foi multado. Aplicar multa de 5 reais por KM acima dos 110
 '''
+
+velocidade = int(input('Qual a velocidade do carro?'))
+
+if velocidade > 110:
+    velocidadeAcima = (velocidade-110)
+    vlrMulta = velocidadeAcima*5
+    print('Você excedeu a velodidade em:',velocidadeAcima,'KM. O valor da sua multa será de : R$',vlrMulta)
+else:
+    print('se livrou.')
