d491cc594df0c394579526a3698952be01d260d1
arosRodrigo <an.rodrigo.os@gmail.com>
2019-07-15 11:34:18 -0300

    add files python para Zumbis

---
+++ Python/para_Zumbis/exec1-radar KM.py
@@ -0,0 +1,4 @@
+'''
+    perguntar a velocidade de um carro. Se o mesmo ultrapassou 110 Km,
+    exibir mensagem informando que o usuário foi multado. Aplicar multa de 5 reais por KM acima dos 110
+'''
