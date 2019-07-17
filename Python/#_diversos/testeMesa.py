720115f8d0ff3f57c82fe6e919b1bece90428585
arosRodrigo <an.rodrigo.os@gmail.com>
2019-07-17 11:15:58 -0300

    alterações de diretórios

--- Python/#_diversos/testeMesa.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-#url ='https://code.launchpad.net/~inkscape.dev/+archive/ubuntu/stable'
-url = 'https://satsp.fazenda.sp.gov.br/COMSAT/Public/ConsultaPublica/ConsultaPublicaCfe.aspx'
-page = requests.get(url)
-print(page)
