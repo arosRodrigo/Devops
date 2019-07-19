9a4fbff39ce008430da7c8beb42a31cf3a1e96e6
arosRodrigo <an.rodrigo.os@gmail.com>
2019-07-19 17:58:08 -0300

    versao 1 reafFile.py

--- Python/import_Catalog/readFile.py
+++ Python/import_Catalog/readFile.py
@@ -1,61 +1,54 @@
 import pandas as pd
 import xlrd
+from datetime import date
+import math
 
-#ler o arquivo
-df = pd.read_excel('Cadastro.xlsx', sheet_names='cad')
-lista = df['NomeProjeto'].tolist()
-maior = max(lista, key=lambda x: len(x.strip()))
-print(len(maior.strip()))
+#--DECLARAÇÕES-----------------------------------------------
+# Metadados
+# Matriz
+matriz = pd.read_excel('Matriz.xlsx', sheet_names='Tabelas')
+# Catalogo
+catalago = pd.read_excel('Catalogo.xlsx', sheet_names='registros')
+# Relatório de auditoria------------------------
+relAudit = ['Arquivo conforme']
 
+#--AUDITORIA-------------------------------------------------
+# nomenclatura
+for i in catalago.columns:
+    colunas = (matriz['NOME LOGICO'].tolist())
+    if i not in colunas: # verifica se a coluna do arquivo consta na matriz
+        relAudit.append('Coluna ' + i + ' não conforme. Verifique a nomenclatura.')
+    else: # verifica o conteúdo da coluna
+        tipo = matriz['TYPE'][colunas.index(i)]
+        # converte o tipo para um elemento comparável---------------------
+        if tipo == 'str': tipo = type(tipo)
+        elif tipo == 'int': tipo = type(0)
+        elif tipo == 'datetime': tipo = type(date.today())
+        # obrigatoriedade---------------------------
+        obrg = matriz['Not null'][colunas.index(i)]
 
-'''
+        # tamanho definido para o campo---------------------------
+        size = matriz['SIZE'][colunas.index(i)]
 
-#--AUDITORIA------------------------------
+        conteudo = (catalago[i].tolist())# aloca em uma lista todo o conteudo da coluna
+        for r in conteudo:# verifica cada registro
+            if obrg == 'Sim': # verifica obrigatoriedade
+                if type(r) == str:
+                    if not r.strip():
+                        relAudit.append('Coluna ' + i + ' não conforme. Verifique o preenhchimento')
+                elif type(r) == int:
+                    if not r:
+                        relAudit.append('Coluna ' + i + ' não conforme. Verifique o preenhchimento')
+                elif type(r) != float:
+                    if math.isnan(r)==True:
+                        relAudit.append('Coluna ' + i + ' não conforme. Verifique o preenhchimento')
 
-#Verificar o cabeçalho
-cabecalho = ['NomeProjeto','DescricaoProjeto','NomeFisico','Owner']
-cabecalhoFile = []
-relAudit = []
-for r in df.columns:
-    cabecalhoFile.append(r.strip())
-
-#Verifica o tipo
-
-#Verificar tamanho de colunas do tipo inteiro
-lista = df['NomeProjeto'].tolist()
-maior = max(lista, key=lambda x: len(x.strip()))
-print(len(maior.strip()))
-
-
-#verificar campos obrigatórios
-
-#Preenche relatório de auditoria
-if cabecalho != cabecalhoFile:
-    relAudit = [{'Cabeçhalho':'não conforme'}]
-else:
-    relAudit = [{'Cabeçhalho':'conforme'}]
-'''
-
-
-'''
-#armazenar em um dicionário
-registros = []
-for i in df.index:
-    registros.append(
-                      {
-                       'NomeProjeto':df['NomeProjeto'][i],
-                       'DescricaoProjeto': df['DescricaoProjeto'][i],
-                       'NomeFisico': df['NomeFisico'][i],
-                       'Owner': df['Owner'][i]
-                       }
-
-                     )
-print(registros)
-'''
-
-
-
-
-
-#auditar o arquivo
+            if type(r) != tipo:# verifica o tipo do valor
+                    relAudit.append('Coluna ' + i + ' não conforme. Verifique o tipo do registro ' + str(r))
+            elif type(r) == str:
+                if len(r) > size:  # verificar o tamanho do arquivo em caso de string
+                    relAudit.append('Coluna ' + i + ' não conforme. Verifique o tamanho do registro ' + str(r))
+#-- FIM AUDITORIA---------------------------------------------------------------------------------
+for i in relAudit:
+    print(i)
 
