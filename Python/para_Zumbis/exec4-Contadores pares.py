e367df56e331e423e768ab20da65a1d180ea6518
arosRodrigo <an.rodrigo.os@gmail.com>
2019-07-22 18:48:38 -0300

    exec4-Contadores pares resolvido 2

--- Python/para_Zumbis/exec4-Contadores pares.py
+++ Python/para_Zumbis/exec4-Contadores pares.py
@@ -3,9 +3,19 @@
 
 '''
 
+''' alternativa 1
 numero = int(input("Digite um numero"))
 i = 2
 
 while i<= numero:
     print(i)
     i = i + 2
+'''
+
+numero = int(input("Digite um numero"))
+i = 0
+
+while i<= numero:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
