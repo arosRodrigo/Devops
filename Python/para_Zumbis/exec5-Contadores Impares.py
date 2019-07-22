65dc4eb82121e4bde6b4c1df1390249469452049
arosRodrigo <an.rodrigo.os@gmail.com>
2019-07-22 18:49:17 -0300

    exec5-Contadores impares adicionado

---
+++ Python/para_Zumbis/exec5-Contadores Impares.py
@@ -0,0 +1,9 @@
+'''
+    imprima apenas os números impares entre 0 e um numero digitado pelo usuário
+
+'''
+
+
+'''
+    reescreva o programa anterior para escrever os 10 primeiros múltiplos  de 3
+'''
